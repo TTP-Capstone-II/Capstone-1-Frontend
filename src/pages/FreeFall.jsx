0cedb007a9c48972af64d670cb8833dd538d5568
Darrell Moreno <dmoreno1987@outlook.com>
2025-08-05 07:25:45 -0400

    Added functionality to allow user to control initial height and velocity when creating the square

--- src/pages/FreeFall.jsx
+++ src/pages/FreeFall.jsx
@@ -2,34 +2,58 @@ import React, { useState } from "react";
 import BaseSimulation from "../components/BaseSimulation";
 import FreeFallInterface from "../interfaces/FreeFallInterface";
 import Matter from "matter-js";
-import { Engine, Render, Bodies, World, Runner, Mouse, MouseConstraint } from "matter-js";
+import {
+  Engine,
+  Render,
+  Bodies,
+  World,
+  Runner,
+  Mouse,
+  MouseConstraint,
+} from "matter-js";
 import { Button } from "@mui/material";
 
 const FreeFall = () => {
-    const [userInput, setUserInput] = useState({
-        gravity: "9.81",
-        initialVelocity: "0",
-        finalVelocity: "",
-        initialHeight: "",
-        finalHeight: "0",
-        time: "",
-        target: "",
+  const [userInput, setUserInput] = useState({
+    gravity: "9.81",
+    initialVelocity: "0",
+    finalVelocity: "",
+    initialHeight: "",
+    finalHeight: "0",
+    time: "",
+    target: "",
+  });
+
+  const handleEngineReady = (engine, world) => {
+    const square = Matter.Bodies.rectangle(
+      200,
+      700 - userInput.initialHeight,
+      50,
+      50
+    );
+
+    Matter.Body.setPosition(square, {
+      x: 500,
+      y: 700 - userInput.initialHeight,
     });
 
-    const handleEngineReady = (engine, world) => {
-        const square = Matter.Bodies.rectangle(200, userInput.initialHeight, 50, 50);
+    Matter.Body.setVelocity(square, {
+      x: 0,
+      y: userInput.initialVelocity,
+    });
 
-        
-        World.add(world, square);
+    World.add(world, square);
+  };
 
-    };
-
-    return (
-        <div className="simulation-page" style={{ display: 'flex', height: '700px' }}>
-            <FreeFallInterface userInput={userInput} setUserInput={setUserInput} />
-            <BaseSimulation onEngineReady={handleEngineReady} />
-        </div>
-    );
+  return (
+    <div
+      className="simulation-page"
+      style={{ display: "flex", height: "700px" }}
+    >
+      <FreeFallInterface userInput={userInput} setUserInput={setUserInput} />
+      <BaseSimulation onEngineReady={handleEngineReady} />
+    </div>
+  );
 };
 
 export default FreeFall;
