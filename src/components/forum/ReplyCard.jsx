f980e30cb9983d46ed62b7ed89b56307b0876859
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-08-05 20:43:12 -0400

    Improved ReplyCard.jsx

--- src/components/forum/ReplyCard.jsx
+++ src/components/forum/ReplyCard.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import { CardHeader } from "@mui/material";
+import { Typography } from "@mui/material";
 
 const ReplyCard = ({ author, content, createdAt }) => {
   return (
-    <Card>
-      <CardHeader>
-        <label>Author: {author}</label>
-        <label>Created At: {createdAt}</label>
-      </CardHeader>
-      <CardContent>{content}</CardContent>
+    <Card
+      sx={{
+        marginBottom: 2,
+        cursor: "pointer",
+        "&:hover": {
+          boxShadow: 3,
+        },
+      }}
+    >
+      <CardContent>
+        <Typography color="text.secondary">
+          {author} - {new Date(createdAt).toLocaleDateString()}
+        </Typography>
+        {content}
+      </CardContent>
     </Card>
   );
 };
