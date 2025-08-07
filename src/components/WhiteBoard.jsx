cf74033bc9a037d1e397494d9575eec9dfafbbba
hailia sommerville <hailiassommerville@gmail.com>
2025-08-06 22:52:05 -0400

    feat: useEffect added that controls canvas size and drawing tool

--- src/components/WhiteBoard.jsx
+++ src/components/WhiteBoard.jsx
@@ -1,7 +1,21 @@
 import React, {useEffect, useRef} from "react";
 
 const WhiteBoard = () => {
-    
+    const canvasRef = useRef(null); // Reference to the canvas element
+    const contextRef = useRef(null); // Reference to the canvas context
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+
+        const context = canvas.getContext("2d");
+        context.lineCap = "round";
+        context.strokeStyle = "black";
+        context.lineWidth = 3;
+
+        contextRef.current = context;
+    }, []);
 }
 
 export default WhiteBoard;
