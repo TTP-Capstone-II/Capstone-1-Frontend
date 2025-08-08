f08d44e20a1d4f9a2b017d22a3688468f8af2b79
hailia sommerville <hailiassommerville@gmail.com>
2025-08-08 16:16:14 -0400

    fix: moved getTracks inside getAudioStream

--- src/components/VoiceChannel.jsx
+++ src/components/VoiceChannel.jsx
@@ -9,10 +9,7 @@ const VoiceChannel = () => {
 
   const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]};
   const peerConnection = new RTCPeerConnection(configuration);// Create a new RTCPeerConnection instance with STUN server configuration
-  stream.getTracks().forEach((track) => { // Add each track from the stream to the peer connection
-    peerConnection.addTrack(track, stream);
-  }
-  );
+  
   peerConnection.ontrack = (event) => { // Handle incoming tracks from the remote peer
     const remoteStream = event.streams[0];
     const audioElement = document.createElement("audio");
@@ -25,6 +22,11 @@ const VoiceChannel = () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
       setStream(stream);
+      
+      stream.getTracks().forEach((track) => { // Add each track from the stream to the peer connection
+        peerConnection.addTrack(track, stream);
+      }
+      );
     } catch (error) {
       console.log("Cannot get audio stream", error);
     }
