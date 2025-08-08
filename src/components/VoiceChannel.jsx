1a29ac4ccab5c82d14a90dc14cbf511a782e7dbf
hailia sommerville <hailiassommerville@gmail.com>
2025-08-08 13:24:44 -0400

    feat:conditional rendering added for audio btns + useStates for connection and mute

--- src/components/VoiceChannel.jsx
+++ src/components/VoiceChannel.jsx
@@ -1,6 +1,24 @@
 import React, { useState } from 'react';
+import { Button } from '@mui/material';
 
 const VoiceChannel = () => {
-    
+    const [isConnected, setIsConnected] = useState(false);
+    const [isMuted, setIsMuted] = useState(false);
+
+
+    return (
+        <div className="voice-channel">
+            {isConnected ? ( 
+                <div className="connected">
+                    <Button >Mute</Button>
+                    <Button >Disconnect Audio</Button>
+                </div>
+            ) : (
+                <div className="disconnected">
+                    <Button >Join Audio</Button>
+                </div>
+            )}
+        </div>
+    );
 }
 export default VoiceChannel;
