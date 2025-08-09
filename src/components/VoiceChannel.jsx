3012b696ce6cf4ac55cf120c452b063c1422cc51
PKhant78 <phonekhantkyawswa555@gmail.com>
2025-08-09 15:57:00 -0400

    pulled from voice-chat

--- src/components/VoiceChannel.jsx
+++ src/components/VoiceChannel.jsx
@@ -7,10 +7,13 @@ const VoiceChannel = () => {
   const [stream, setStream] = useState();
   const [audioDevices, setAudioDevices] = useState();
 
-  const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]};
-  const peerConnection = new RTCPeerConnection(configuration);// Create a new RTCPeerConnection instance with STUN server configuration
-  
-  peerConnection.ontrack = (event) => { // Handle incoming tracks from the remote peer
+  const configuration = {
+    iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
+  };
+  const peerConnection = new RTCPeerConnection(configuration); // Create a new RTCPeerConnection instance with STUN server configuration
+
+  peerConnection.ontrack = (event) => {
+    // Handle incoming tracks from the remote peer
     const remoteStream = event.streams[0];
     const audioElement = document.createElement("audio");
     audioElement.srcObject = remoteStream;
@@ -22,28 +25,19 @@ const VoiceChannel = () => {
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
       setStream(stream);
-      
-      stream.getTracks().forEach((track) => { // Add each track from the stream to the peer connection
+
+      stream.getTracks().forEach((track) => {
+        // Add each track from the stream to the peer connection
         peerConnection.addTrack(track, stream);
-      }
-      );
+      });
     } catch (error) {
       console.log("Cannot get audio stream", error);
     }
   };
 
-  // In case you want to access the devices programmatically. Otherwise, it's unnecessary.
-  /*  const getAudioDevices = async () => {
-    const devices = await navigator.mediaDevices.enumerateDevices();
-    return devices.filter((device) => device.kind === "audioinput");
-  };*/
-
   const handleJoinAudio = async () => {
     try {
       await getAudioStream();
-      //const devices = await getAudioDevices();
-      //setAudioDevices(devices);
-      //console.log("Audio found:", devices);
       setIsConnected(true);
     } catch (error) {
       console.log(error);
