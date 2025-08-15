33a7d804f098fa1773af3e3e60944defb8baefc6
Darrell Moreno <weaselzero@gmail.com>
2025-08-15 10:36:50 -0400

    Started Websocket implementation for forums and posts. STILL MAKING IT.

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -4,34 +4,58 @@ import { useParams } from "react-router";
 import { API_URL } from "../shared";
 import ReplyList from "../components/forum/ReplyList";
 import ReplyForm from "../forms/ReplyForm";
+import socket from "../socket";
 
 const PostPage = ({ user }) => {
   const [post, setPost] = useState([]);
-  const { forumId } = useParams();
+  const [replies, setReplies] = useState([]);
+  const { forumId, postId } = useParams();
   let params = useParams();
+
   const fetchPost = async () => {
     try {
       const response = await axios.get(
         `${API_URL}/api/forum/${forumId}/posts/${params.postId}`
       );
       setPost(response.data);
+      //setReplies(response.data.replies || []);
       console.log(response.data);
     } catch (error) {
       console.error("Error fetching posts:", error);
     }
   };
 
+  const fetchReplies = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/api/post/${params.postId}/reply`);
+    setReplies(response.data);
+  } catch (error) {
+    console.error("Error fetching replies:", error);
+  }
+};
+
   useEffect(() => {
     fetchPost();
-  }, []);
+    fetchReplies();
+
+    socket.emit("join-post", postId);
+
+    socket.on("new-reply", (reply) => {
+      setReplies((prev) => [...prev, reply]);
+    });
+
+    return () => {
+      socket.off("new-reply");
+    };
+  }, [postId]);
 
   return (
     <div className="forum-page">
       <h1>Post</h1>
       <div>{post.title}</div>
       <div>{post.content}</div>
-      <ReplyList postId={post.id} userId={user?.id} />
-      <ReplyForm postId={post.id} userId={user?.id} />
+      <ReplyList replies={replies} userId={user?.id} />
+      <ReplyForm postId={postId} userId={user?.id} />
     </div>
   );
 };
